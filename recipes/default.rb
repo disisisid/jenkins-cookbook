fa198cd6722e9cdb6ac13324ce86c1aba9362857
disisisid <sidnshady@gmail.com>
2018-10-12 21:41:58 +0800

    Initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+# wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+
+# sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+# sudo apt-get update
+# sudo apt-get install jenkins
