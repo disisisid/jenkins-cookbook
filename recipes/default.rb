f9bd103106d1f77c427920bbc62a6d6d32199bc7
disisisid <sidnshady@gmail.com>
2018-10-13 00:41:56 +0800

    Installing jenkins with package

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,25 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 # wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+remote_file '/tmp/jenkins.io.key' do
+	source 'https://pkg.jenkins.io/debian/jenkins.io.key'
+	notifies :run, 'execute[apt-key add /tmp/jenkins.io.key]', :immediately
+end
+
+execute 'apt-key add /tmp/jenkins.io.key' do
+	action :nothing
+end
 
 # sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+file '/etc/apt/sources.list.d/jenkins.list' do
+	content 'deb http://pkg.jenkins.io/debian-stable binary/'
+	notifies :run, 'execute[apt update]', :immediately
+end
+
 # sudo apt-get update
+execute 'apt update' do
+	action :nothing
+end
+
 # sudo apt-get install jenkins
+package 'jenkins'
